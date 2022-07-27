87f79c88b87c592b92c8af3a552c31162720fb4e
bbora <>
2022-07-27 16:43:51 +0900

    2022.07.26 - 6002. 출력하기02 ~ 6005. 출력하기05

--- codeup_basic100/6001.출력하기.playground/Contents.swift
+++ codeup_basic100/6001.출력하기.playground/Contents.swift
@@ -1,3 +1,18 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//6001
+print("Hello")
+
+//6002
+print("Hello World")
+print("Hello","World")
+
+//6003
+print("Hello")
+print("World")
+
+//6004
+print("'Hello'")
+
+//6005
+print("\"Hello\"")
