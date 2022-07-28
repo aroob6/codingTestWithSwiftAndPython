bfb745b9a05327eb6fb22f117a8caed7c5126575
bbora <>
2022-07-28 16:17:16 +0900

    2022.07.28 - 6006 ~ 6008. 출력하기05

--- codeup_basic100/6001.출력하기.playground/Contents.swift
+++ codeup_basic100/6001.출력하기.playground/Contents.swift
@@ -16,3 +16,16 @@ print("'Hello'")
 
 //6005
 print("\"Hello\"")
+
+//6006
+//"!@#$%^&*()'
+//(단, 큰따옴표와 작은따옴표도 함께 출력한다.)
+print("\"!@#$%^&*()'")
+
+//6007
+//"C:\Download\'hello'.py"
+print("\"C:\\Download\\'hello'.py\"")
+
+//6008
+//print("Hello\nWorld")
+print("print(\"Hello\\nWorld\")")
