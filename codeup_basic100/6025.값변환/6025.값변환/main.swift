786d1b9379564b5afed27aa72e00a621d7b9a148
bbora <>
2022-07-30 19:42:57 +0900

    2022.07.30 - 6026 [기초-값변환] 까지

---
+++ codeup_basic100/6025.값변환/6025.값변환/main.swift
@@ -0,0 +1,32 @@
+//
+//  main.swift
+//  6025.값변환
+//
+//  Created by 이보라 on 2022/07/30.
+//
+
+import Foundation
+
+//6025
+//let a = readLine()
+//
+//if let a = a {
+//    let split = a.split(separator: " ")
+//
+//    if let num1 = Int(split[0]), let num2 = Int(split[1]) {
+//        let res = num1 + num2
+//        print(res)
+//    }
+//}
+
+//6026
+let a = readLine()
+let b = readLine()
+
+if let a = a, let b = b{
+    if let num1 = Float(a), let num2 = Float(b) {
+        let res = num1 + num2
+        print(res)
+    }
+}
+
