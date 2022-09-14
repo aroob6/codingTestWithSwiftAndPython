b906ad3cc658e4b75efa53fe82121df734d1d885
bbora <>
2022-09-14 15:01:29 +0900

    2022.09/14 - 나머지가 1이 되는 수 찾기

--- programmers/programmersLevel1/programmersLevel1/main.swift
+++ programmers/programmersLevel1/programmersLevel1/main.swift
@@ -7,5 +7,15 @@
 
 import Foundation
 
-print("Hello, World!")
+// 나머지가 1이 되는 수 찾기
+func solution(_ n:Int) -> Int {
+    for i in 1 ..< n {
+        if n % i == 1 {
+            return i
+        }
+    }
+     return 0
+}
 
+let a = Int(readLine()!)!
+print(solution(a))
