291b7ff7b8b50d96adbeaa49bca23c40e5ca492a
bbora <>
2022-09-14 15:04:53 +0900

    2022.09.14 - 짝수와 홀수

--- programmers/programmersLevel1/programmersLevel1/main.swift
+++ programmers/programmersLevel1/programmersLevel1/main.swift
@@ -8,13 +8,25 @@
 import Foundation
 
 // 나머지가 1이 되는 수 찾기
-func solution(_ n:Int) -> Int {
-    for i in 1 ..< n {
-        if n % i == 1 {
-            return i
-        }
+//func solution(_ n:Int) -> Int {
+//    for i in 1 ..< n {
+//        if n % i == 1 {
+//            return i
+//        }
+//    }
+//     return 0
+//}
+
+// 짝수와 홀수
+func solution(_ num:Int) -> String {
+    if num == 0 {
+        return "Even"
     }
-     return 0
+    else if num % 2 == 0 {
+        return "Even"
+    }
+    
+    return "Odd"
 }
 
 let a = Int(readLine()!)!
