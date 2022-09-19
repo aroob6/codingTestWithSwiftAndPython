f38706282c187aeea914e5a1b239070c331623de
bbora <>
2022-09-19 15:19:02 +0900

    2022.09.19 - 자릿수 더하기 ~ 문자열 내 p와 y의 개수

--- programmers/programmersLevel1/programmersLevel1/main.swift
+++ programmers/programmersLevel1/programmersLevel1/main.swift
@@ -28,14 +28,102 @@ import Foundation
 //
 //    return "Odd"
 //}
+
+//자릿수 더하기
+//func solution(_ n:Int) -> Int
+//{
+//    var answer:Int = 0
 //
-//let a = Int(readLine()!)!
-//print(solution(a))
+//    _ = String(n).map {
+//        answer += Int(String($0))!
+//    }
+//
+//    return answer
+//}
 
+//약수의 합
+//func solution(_ n:Int) -> Int {
+//    var sum = 0
+//    if n == 0 {
+//        return 0
+//    }
+//    else {
+//        for i in 1 ... n {
+//            if n % i == 0 {
+//                sum += i
+//            }
+//        }
+//        return sum
+//    }
+//}
 
-for i in 0 ..< 10 {
-    for _ in 0 ..< i {
-        print("*", terminator: "")
+//평균 구하기
+//func solution(_ arr:[Int]) -> Double {
+//    var sum = 0
+//    var avg = 0.0
+//
+//    for i in arr {
+//        sum += i
+//    }
+//
+//    avg = Double(sum) / Double(arr.count)
+//
+//    return avg
+//}
+//print(solution([1,2,3]))
+
+//정수 제곱근 판별
+//func solution(_ n:Int64) -> Int64 {
+//    let sqrtN = Int64(sqrt(Double(n)))
+//
+//    if (sqrtN * sqrtN) == n {
+//        return (sqrtN + 1) * (sqrtN + 1)
+//    }
+//
+//    return -1
+//}
+
+//자연수 뒤집어 배열로 만들기
+//func solution(_ n:Int64) -> [Int] {
+//    var res: [Int] = []
+//
+//    _ = String(n).map {
+//        res.append(Int(String($0))!)
+//    }
+//
+//    return res.reversed()
+//}
+
+//정수 내림차순
+//func solution(_ n:Int64) -> Int64 {
+//    var res = ""
+//    _ = String(n).sorted(by: >).map{ res.append($0)}
+//    return Int64(res)!
+//}
+
+//문자열 내 p와 y의 개수
+func solution(_ s:String) -> Bool
+{
+    var ans:Bool = false
+    var cntP = 0
+    var cntY = 0
+    
+    _ = s.lowercased().map{
+        if  String($0) == "p" {
+            cntP += 1
+        }
+        if String($0) == "y" {
+            cntY += 1
+        }
     }
-    print("")
+    
+    ans = cntP == cntY ? true : false
+
+    return ans
 }
+let a = readLine()!
+print(solution(a))
+
+//let a = Int(readLine()!)!
+//print(solution(Int64(a)))
+
