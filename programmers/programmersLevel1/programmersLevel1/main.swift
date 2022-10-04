755d64375054ef203025767dad6dafec8f6b8ad9
bbora <>
2022-10-04 16:47:07 +0900

    2022.10.04 - 약수의 개수와 덧셈

--- programmers/programmersLevel1/programmersLevel1/main.swift
+++ programmers/programmersLevel1/programmersLevel1/main.swift
@@ -335,15 +335,35 @@ import Foundation
 //print(solution("Zbcdefg"))
 
 //문자열 다루기 기본
-func solution(_ s:String) -> Bool {
-//    return (Int(s) != nil && (s.count == 4 || s.count == 6)) ? true : false
-    if s.count == 4 || s.count == 6 {
-        guard let _ = Int(s) else {
-            return false
+//func solution(_ s:String) -> Bool {
+////    return (Int(s) != nil && (s.count == 4 || s.count == 6)) ? true : false
+//    if s.count == 4 || s.count == 6 {
+//        guard let _ = Int(s) else {
+//            return false
+//        }
+//    }
+//    else {
+//        return false
+//    }
+//    return true
+//}
+
+//약수의 개수와 덧셈
+func solution(_ left:Int, _ right:Int) -> Int {
+    //    return (left...right).map { i in (1...i).filter { i % $0 == 0 }.count % 2 == 0 ? i : -i }.reduce(0, +)
+    var res = 0
+    for i in left ... right {
+        var cnt = 0
+        for j in 1 ... i {
+            if i % j == 0 {
+                cnt += 1
+            }
         }
+        
+        res = cnt % 2 == 0 ? res + i : res - i
     }
-    else {
-        return false
-    }
-    return true
+    
+    return res
 }
+
+print(solution(13, 17))
