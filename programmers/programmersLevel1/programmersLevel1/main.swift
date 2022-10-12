f6e3620b7bb89312656acc10e0829f2ca446f3c3
bbora <>
2022-10-12 16:45:41 +0900

    2022.10.12 - 예산

--- programmers/programmersLevel1/programmersLevel1/main.swift
+++ programmers/programmersLevel1/programmersLevel1/main.swift
@@ -461,3 +461,32 @@ import Foundation
 //    return Int(String(String(n, radix: 3).reversed()), radix: 3)!
 //}
 //print(solution(125))
+
+//예산
+func solution(_ d:[Int], _ budget:Int) -> Int {
+//    var budget = budget
+//
+//    return d.sorted().filter{
+//        budget = budget - $0
+//        return budget >= 0
+//    }.count
+    
+    var cnt = 0
+    var sum = 0
+    
+    for i in d.sorted() {
+        sum += i
+        
+        if sum == budget {
+            cnt += 1
+            break
+        }
+        else if sum > budget { break }
+        else { cnt += 1 }
+    }
+    
+    return cnt
+}
+
+print(solution([1,3,2,5,4], 9))
+print(solution([2,2,3,3], 10))
