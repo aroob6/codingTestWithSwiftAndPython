ac8bf69fe0b09368368c69bed6d9beb3cf442ba5
bbora <>
2022-10-28 17:09:05 +0900

    2022.10.28 - 숫자 문자열과 영단어

--- programmers/programmersLevel1/programmersLevel1/main.swift
+++ programmers/programmersLevel1/programmersLevel1/main.swift
@@ -600,19 +600,91 @@ import Foundation
 //print(solution(["sun", "bed", "car"], 1))
 //print(solution(["abce", "abcd", "cdx"], 2))
 
-//K번째 수 
-func solution(_ array:[Int], _ commands:[[Int]]) -> [Int] {
-//    return commands.map{ (key) in
-//        return array[(key[0]-1)...(key[1]-1)].sorted()[key[2]-1]
+//K번째 수
+//func solution(_ array:[Int], _ commands:[[Int]]) -> [Int] {
+////    return commands.map{ (key) in
+////        return array[(key[0]-1)...(key[1]-1)].sorted()[key[2]-1]
+////    }
+//    return commands.map { arr in
+//        let i = arr[0] - 1
+//        let j = arr[1] - 1
+//        let k = arr[2] - 1
+//        let slice = array[i...j].sorted()
+//
+//        return slice[k]
 //    }
-    return commands.map { arr in
-        let i = arr[0] - 1
-        let j = arr[1] - 1
-        let k = arr[2] - 1
-        let slice = array[i...j].sorted()
+//}
+//
+//print(solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]]))
 
-        return slice[k]
+//숫자 문자열과 영단어
+func solution(_ s:String) -> Int {
+//    var s = s
+//            var answer = s.replacingOccurrences(of: "zero", with: "0")
+//                .replacingOccurrences(of: "one", with: "1")
+//                .replacingOccurrences(of: "two", with: "2")
+//                .replacingOccurrences(of: "three", with: "3")
+//                .replacingOccurrences(of: "four", with: "4")
+//                .replacingOccurrences(of: "five", with: "5")
+//                .replacingOccurrences(of: "six", with: "6")
+//                .replacingOccurrences(of: "seven", with: "7")
+//                .replacingOccurrences(of: "eight", with: "8")
+//                .replacingOccurrences(of: "nine", with: "9")
+//
+//        return Int(answer)!
+    if let resNum = Int(s) {
+        return resNum
+    }
+    else {
+        var res = ""
+        var strNum = ""
+        
+        _ = s.map { ch in
+            if let num = Int(String(ch)) {
+                res += String(num)
+            }
+            else {
+                strNum += String(ch)
+            }
+            
+            switch strNum {
+            case "zero":
+                res += "0"
+                strNum = ""
+            case "one":
+                res += "1"
+                strNum = ""
+            case "two":
+                res += "2"
+                strNum = ""
+            case "three":
+                res += "3"
+                strNum = ""
+            case "four":
+                res += "4"
+                strNum = ""
+            case "five":
+                res += "5"
+                strNum = ""
+            case "six":
+                res += "6"
+                strNum = ""
+            case "seven":
+                res += "7"
+                strNum = ""
+            case "eight":
+                res += "8"
+                strNum = ""
+            case "nine":
+                res += "9"
+                strNum = ""
+            default: break
+            }
+        }
+        
+        return Int(res)!
     }
 }
 
-print(solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]]))
+print(solution("123onezero"))
+print(solution("123"))
