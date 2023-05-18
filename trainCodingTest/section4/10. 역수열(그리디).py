ec306bdaa42c9c2d37fd5a0441fc9a4316082a1b
bbora <aroxb6@gmail.com>
2023-05-18 23:41:41 +0900

    으렵다 으려워

--- trainCodingTest/section4/10. 역수열(그리디).py
+++ trainCodingTest/section4/10. 역수열(그리디).py
@@ -4,10 +4,10 @@ def solution(n, arr):
     for i in range(n):
         for j in range(n):
             print(i, j, arr,seq)
-            if arr[i] == 0 and seq[j] == 0: 
+            if arr[i] == 0 and seq[j] == 0: # 내 자리 찾으면 넣기
                 seq[j] = i+1
                 break
-            elif seq[j] == 0:
+            elif seq[j] == 0: #자리 찾을 때까지 실행
                 arr[i] -= 1
 
     print(seq)
