5960bcc82a8c2c654dcfd1fa2480b4c0cdb91b65
bbora <aroxb6@gmail.com>
2023-05-23 22:30:28 +0900

    코테연습 섹션5 최소힙, 최대힙

---
+++ trainCodingTest/section5/11. 최대힙.py
@@ -0,0 +1,20 @@
+import heapq as hq
+
+def solution():
+    a = []
+    while True:
+        print(a)
+        n = int(input())
+        if n == -1:
+            break
+        if n == 0:
+            if len(a) == 0:
+                print(-1)
+            else:
+                print(-hq.heappop(a))
+        else:
+            hq.heappush(a, -n)
+            
+    return "종료"
+
+print(solution())
