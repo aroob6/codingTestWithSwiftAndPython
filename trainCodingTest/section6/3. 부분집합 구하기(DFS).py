80cf3487d3c0dc25e256b7fef147ee1f84eae6f4
bbora <aroxb6@gmail.com>
2023-05-27 23:10:37 +0900

    코테연습 섹션6 부분집합구하기(DFS)

---
+++ trainCodingTest/section6/3. 부분집합 구하기(DFS).py
@@ -0,0 +1,18 @@
+from itertools import combinations 
+def solution(n):
+    ch = [0] * (n+1)
+    def DFS(x):
+        if x == n+1:
+            for i in range(1,n+1):
+                if ch[i] == 1:
+                    print (i, end=" ")
+            print()
+        else:
+            ch[x] = 1
+            DFS(x+1)
+            ch[x] = 0
+            DFS(x+1)
+    DFS(1)
+    return "끝"
+
+print(solution(3))
