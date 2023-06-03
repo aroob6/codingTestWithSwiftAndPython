cae648c194bb4158c57db0a6a7534ee59570d2fb
bbora <aroxb6@gmail.com>
2023-06-03 20:47:55 +0900

    코테연습 섹션6 순열추측하기 라이브러리 사용

--- trainCodingTest/section6/9. 순열추측하기.py
+++ trainCodingTest/section6/9. 순열추측하기.py
@@ -1,30 +1,47 @@
 import sys
-def DFS(L, sum):
-    if L == n and sum == f:
-        for i in p:
-            print(i, end=" ")
-        sys.exit(0)
-    else:
-        for i in range(1,n+1):
-            if num[i] == 0:
-                num[i] = 1
-                p[L] = i
-                DFS(L+1, sum+(p[L]*b[L])) # 여기 아래 부분은 백하고 온 후 실행
-                num[i] = 0
+import itertools as it
+# def DFS(L, sum):
+#     if L == n and sum == f:
+#         for i in p:
+#             print(i, end=" ")
+#         sys.exit(0)
+#     else:
+#         for i in range(1,n+1):
+#             if num[i] == 0:
+#                 num[i] = 1
+#                 p[L] = i
+#                 DFS(L+1, sum+(p[L]*b[L])) # 여기 아래 부분은 백하고 온 후 실행
+#                 num[i] = 0
 
-if __name__ == "__main__":
-    n = 4
-    f = 16
-    num = [0] * (n+1)
-    p = [0] * n
-    b = [0] * n
+# if __name__ == "__main__":
+#     n = 4
+#     f = 16
+#     num = [0] * (n+1)
+#     p = [0] * n
+#     b = [0] * n
 
-    for i in range(n):
-        if i == 0 or i == n-1:
-            b[i] = 1
-        else:
-            b[i] = 3
-    DFS(0, 0)
+#     for i in range(n):
+#         if i == 0 or i == n-1:
+#             b[i] = 1
+#         else:
+#             b[i] = 3
+#     DFS(0, 0)
 
+def solution(n,f):
+    b = [1]*n
+    for i in range(1,n):
+        b[i] = b[i-1]*(n-i)/i
+    arr = list(range(1,n+1))
 
+    for tmp in it.permutations(arr):
+        sum = 0
+        for L, x in enumerate(tmp):
+            sum += (x*b[L])
+        if sum == f:
+            for x in tmp:
+                print(x, end=' ')
+            break
 
+    return 0
+
+print(solution(4,16))
