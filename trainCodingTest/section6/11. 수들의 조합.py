ceb3c38e8ee6793369db100f4d2012df273e6eb5
bbora <aroxb6@gmail.com>
2023-06-03 20:57:35 +0900

    코테연습 섹션6 순열추측하기 라이브러리 사용

--- trainCodingTest/section6/11. 수들의 조합.py
+++ trainCodingTest/section6/11. 수들의 조합.py
@@ -1,16 +1,28 @@
-def DFS(L,s,sum):
-    global cnt
-    if L == k and sum % m == 0:
-        cnt += 1
-    else:
-        for i in range(s,n):
-            DFS(L+1, i+1, sum+arr[i])
+import itertools as it
+# def DFS(L,s,sum):
+#     global cnt
+#     if L == k and sum % m == 0:
+#         cnt += 1
+#     else:
+#         for i in range(s,n):
+#             DFS(L+1, i+1, sum+arr[i])
 
-if __name__ == "__main__":
-    n = 5
-    k = 3
-    arr = [2,4,5,8,12]
-    m = 6
+# if __name__ == "__main__":
+#     n = 5
+#     k = 3
+#     arr = [2,4,5,8,12]
+#     m = 6
+#     cnt = 0
+#     DFS(0,0,0)
+#     print(cnt)
+
+def solution(n,k,arr,m):
     cnt = 0
-    DFS(0,0,0)
-    print(cnt)
+
+    for x in it.combinations(arr,k):
+        if sum(x)%m == 0:
+            cnt += 1
+
+    return cnt
+
+print(solution(5,3,[2,4,5,8,12],6))
